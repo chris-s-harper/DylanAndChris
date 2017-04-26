022e191e835972bf9df9fa32a95c09741824e873
CHRIS-PC\cshar <chris.harper@loop.colum.edu>
2017-04-26 12:59:24 -0500

    Animated PowerUp Cube and added color material. Also Added input settings to include boost button for each controller

--- Projcect1/Assets/Scripts/RestoreBoosts.cs
+++ Projcect1/Assets/Scripts/RestoreBoosts.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RestoreBoosts : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+public class RestoreBoosts : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(0, 5, 0,Space.World);
+        gameObject.transform.Translate(0, 0.1f, 0, Space.World);
+        gameObject.transform.Translate(0, -0.1f, 0, Space.World);
+    }
+    // Update is called once per frame
+    void Update ()
+    {
 	
 	}
 
